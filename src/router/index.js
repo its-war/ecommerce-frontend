f2318c1a6dbe9b828bc3336f475e6b9987e68b82
Karlos Warney <karloswarney@gmail.com>
2023-03-17 21:01:56 -0300

    ADD - Rotas principais criadas e funcionando.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from "@/views/HomePage.vue";
+import HomePage from "@/views/HomeView.vue";
+import InicioComponent from "@/components/pages/InicioPage.vue";
+import AtendimentoComponent from "@/components/pages/AtendimentoPage.vue";
+import MaisVendidosPage from "@/components/pages/MaisVendidosPage.vue";
+import LancamentosPage from "@/components/pages/LancamentosPage.vue";
+import PromocoesPage from "@/components/pages/PromocoesPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomePage
+      component: HomePage,
+      children: [
+        {
+          path: '',
+          name: 'Página Inicial',
+          component: InicioComponent
+        },
+        {
+          path: 'atendimento',
+          name: 'Atendimento',
+          component: AtendimentoComponent
+        },
+        {
+          path: 'maisVendidos',
+          name: 'Mais Vendidos',
+          component: MaisVendidosPage
+        },
+        {
+          path: 'lancamentos',
+          name: 'Lançamentos',
+          component: LancamentosPage
+        },
+        {
+          path: 'promocoes',
+          name: 'Promoções',
+          component: PromocoesPage
+        }
+      ]
     },
     {
       path: '/about',
