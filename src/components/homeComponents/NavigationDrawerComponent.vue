a97e557cf8c33ad9fe3bfc23b9ec42801d0e4e03
Karlos Warney <karloswarney@gmail.com>
2023-03-17 21:04:13 -0300

    ALTER - Botões de navegação funcionando e apontando para suas devidas rotas.

--- src/components/homeComponents/NavigationDrawerComponent.vue
+++ src/components/homeComponents/NavigationDrawerComponent.vue
@@ -3,11 +3,11 @@
           handler: clickOutSide
         }">
     <v-list color="transparent">
-      <v-list-item prepend-icon="mdi-home" title="Início"></v-list-item>
-      <v-list-item prepend-icon="mdi-alert-decagram" title="Lançamentos"></v-list-item>
-      <v-list-item prepend-icon="mdi-cash-multiple" title="Mais Vendidos"></v-list-item>
-      <v-list-item prepend-icon="mdi-elevation-decline" title="Promoção"></v-list-item>
-      <v-list-item prepend-icon="mdi-comment-account" title="Atendimento"></v-list-item>
+      <v-list-item :class="$route.path === '/' ? 'page-active' : ''" @click="$router.push({path: '/'})" prepend-icon="mdi-home" title="Início"></v-list-item>
+      <v-list-item :class="$route.path === '/lancamentos' ? 'page-active' : ''" @click="$router.push({path: '/lancamentos'})" prepend-icon="mdi-alert-decagram" title="Lançamentos"></v-list-item>
+      <v-list-item :class="$route.path === '/maisVendidos' ? 'page-active' : ''" @click="$router.push({path: '/maisVendidos'})" prepend-icon="mdi-cash-multiple" title="Mais Vendidos"></v-list-item>
+      <v-list-item :class="$route.path === '/promocoes' ? 'page-active' : ''" @click="$router.push({path: '/promocoes'})" prepend-icon="mdi-elevation-decline" title="Promoção"></v-list-item>
+      <v-list-item :class="$route.path === '/atendimento' ? 'page-active' : ''" @click="$router.push({path: '/atendimento'})" prepend-icon="mdi-comment-account" title="Atendimento"></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -25,10 +25,15 @@ export default {
     clickOutSide(){
       this.$emit('closeDrawer');
     }
+  },
+  mounted() {
+    console.log(this.$route);
   }
 }
 </script>
 
 <style scoped>
-
+.page-active {
+  border-left: 3px solid #FFFFFF;
+}
 </style>
